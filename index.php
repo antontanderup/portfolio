de2a2439bc86f4903fd813bbd199dfb9f8453576
Anton Tanderup <antontandrup@gmail.com>
2014-06-05 15:27:58 +0200

    Added index (portefølje)

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php $page_title = "Portefølje" ?>
+<?php include("header.php");?>
+
+	<!-- Min Profil Start -->
+   <div class="portfolio">
+	<section class="wrap">
+
+		<div class="intro">
+			<h1 class="title">Min Portefølje</h1>
+			<section class="intro-text">
+				<p>Velkommen til min portefølje. Her kan du se projekter jeg har arbejdet på indenfor web-design, front-end udvikling og en smule fotografi.</p>			
+			</section>
+		</div> <!-- intro-text END -->
+
+		<article class="about">
+			<h2 class="subhead">Om mig</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis ipsum ac leo ultricies sagittis in faucibus nibh. Morbi id purus vel nisl sollicitudin venenatis ut ut risus. Curabitur tortor neque, aliquet sed convallis in, tempor et nisi. Phasellus ultrices congue mi ac eleifend. Fusce augue est, posuere nec iaculis eget, faucibus at lacus. Phasellus nunc libero, cursus sit amet tempor lacinia, adipiscing laoreet elit.</p>
+			<p>Proin facilisis eu lacus cursus tempus. Nulla lobortis, dui vel accumsan molestie, nisi mauris volutpat ligula, tincidunt mattis enim odio eget sapien. Donec vestibulum id enim eu ullamcorper. Nunc et risus non eros ultricies tempus.</p>
+			<p>Phasellus nunc libero, cursus sit amet tempor lacinia, adipiscing laoreet elit. Proin facilisis eu lacus cursus tempus. Nulla lobortis, dui vel accumsan molestie, nisi mauris volutpat ligula, tincidunt mattis enim odio eget sapien. Donec vestibulum id enim eu ullamcorper. Nunc et risus non eros ultricies tempus.</p>
+		</article> <!-- about END -->
+
+	</section> <!-- wrap END -->
+
+   </div> <!-- min-profil END -->
+
+
+<?php include("footer.php");?>
