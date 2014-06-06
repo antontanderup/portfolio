d5b12f5de974f20822080c6113eda1ae3c503234
Anton Tanderup <antontandrup@gmail.com>
2014-06-06 20:04:54 +0200

    Social buttons added

--- kontakt.php
+++ kontakt.php
@@ -14,19 +14,25 @@
 		</div> <!-- intro-text END -->
 
 		<section class="contact-form">
+			<h2 class="subhead">Send en besked</h2>
 			<form>
-				<label>Dit navn
-					<input type="tekst" name="name">
-				</label>
-				<label>Din email
-					<input type="email" name="email">
-				</label>
-				<label>Din besked
-					<textarea name="message"></textarea>
-				</label>
-				<input type="submit" value="Send" />
+				<label for="name">Dit navn</label>
+				<input type="tekst" id="name" name="name">				
+				<label for="email">Din email</label>
+				<input type="email" id="email" name="email">			
+				<label for="message">Din besked</label>
+				<textarea name="message" id="message"></textarea>			
+				<input type="submit" value="Send" class="button" />
 			</form>
 		</section> <!-- contact-form END -->
+
+		<aside class="social">
+			<h2 class="subhead">Sociale netværk</h2>
+			<a class="social-button" href="https://www.linkedin.com/pub/anton-tanderup/48/40b/966"><span><?php include("icons/linkedin.svg");?></span></a>
+			<a class="social-button" href="https://flic.kr/ps/vubFe"><span><?php include("icons/flickr.svg");?></span></a>
+			<a class="social-button" href="https://twitter.com/AntonTanderup"><span><?php include("icons/twitter.svg");?></span></a>
+			<a class="social-button" href="https://plus.google.com/+AntonTanderup/"><span><?php include("icons/googleplus.svg");?></span></a>
+		</aside> <!-- social END -->
 
 
 	</section> <!-- wrap END -->
