2de0cbf17f4062384adf4a32ab716fb2a26841a0
Anton Tanderup <antontandrup@gmail.com>
2014-06-11 22:07:07 +0200

    Contact page now responsive

--- kontakt.php
+++ kontakt.php
@@ -1,18 +1,28 @@
 <?php $page_title = "Kontakt mig" ?>
 <?php include("header.php");?>
 
-	<!-- Min Profil Start -->
-   <div class="portfolio">
+	<!-- kontakt Start -->
+   <div class="kontakt-side">
 
 	<section class="wrap">
+
+		<aside class="social">
+			<img src="img/antontanderup.jpg" alt="Anton Tanderup" class="profile-picture" />	
+			<h2 class="subhead">Sociale netværk</h2>
+			<a class="social-button" href="https://www.linkedin.com/pub/anton-tanderup/48/40b/966"><span><?php include("icons/linkedin.svg");?></span></a>
+			<a class="social-button" href="https://flic.kr/ps/vubFe"><span><?php include("icons/flickr.svg");?></span></a>
+			<a class="social-button" href="https://twitter.com/AntonTanderup"><span><?php include("icons/twitter.svg");?></span></a>
+			<a class="social-button" href="https://plus.google.com/+AntonTanderup/"><span><?php include("icons/googleplus.svg");?></span></a>
+			<a class="social-button" href="https://www.facebook.com/antontanderup/"><span><?php include("icons/facebook.svg");?></span></a>
+		</aside> <!-- social END -->
 
 		<div class="intro">
 			<h1 class="title">Kontakt mig</h1>
 			<section class="intro-text">
-				<p>Jeg kan kontaktes ved brug af formularen nedenfor, på email eller telefon.</p>			
+				<p>Jeg kan kontaktes ved brug af formularen nedenfor, på email <a href="mailto:antontandrup@gmail.com">antontandrup@gmail.com</a> eller telefon <a href="tel:28730450">28 73 04 50</a>.</p>			
 			</section>
 		</div> <!-- intro-text END -->
-
+	
 		<section class="contact-form">
 			<h2 class="subhead">Send en besked</h2>
 			<form>
@@ -26,18 +36,9 @@
 			</form>
 		</section> <!-- contact-form END -->
 
-		<aside class="social">
-			<h2 class="subhead">Sociale netværk</h2>
-			<a class="social-button" href="https://www.linkedin.com/pub/anton-tanderup/48/40b/966"><span><?php include("icons/linkedin.svg");?></span></a>
-			<a class="social-button" href="https://flic.kr/ps/vubFe"><span><?php include("icons/flickr.svg");?></span></a>
-			<a class="social-button" href="https://twitter.com/AntonTanderup"><span><?php include("icons/twitter.svg");?></span></a>
-			<a class="social-button" href="https://plus.google.com/+AntonTanderup/"><span><?php include("icons/googleplus.svg");?></span></a>
-		</aside> <!-- social END -->
-
-
 	</section> <!-- wrap END -->
 
-   </div> <!-- min-profil END -->
+   </div> <!-- kontakt-side END -->
 
 
 <?php include("footer.php");?>
