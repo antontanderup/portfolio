16cb7d0bb02da17f123a676f5702d64e37635cc9
Anton Tanderup <antontandrup@gmail.com>
2014-06-11 22:23:13 +0200

    lightbox handler added

--- js/behavior.js
+++ js/behavior.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+
+	// Toggle to hide and show navigation on smaller viewports
 	var navToggle = function() {
 		var navLinks = document.getElementById("top-nav-links");
 		if (navLinks.className === "nav-show") {
@@ -7,6 +9,25 @@ window.onload = function() {
 			navLinks.className = "nav-show";
 		}
 	}
+	// Eventhandler for navigation toggle
 	var navButton = document.getElementById("nav-button");
 	navButton.addEventListener("click", navToggle, false);
+
+	// Function for displaying larger images
+	var displayBox = function(evt) {
+    	evt.preventDefault();
+    	var lightBox = document.getElementById("lightbox");
+    	lightBox.innerHTML = '<img src="' + this + '" alt="image-large"/>';
+    	lightBox.className = 'lightbox-show';
+    	lightBox.addEventListener("click", function() {
+    		lightBox.className ="lightbox-hide";
+    	}, false);
+	}
+
+	// Eventhandler to display larger images. 
+	var img = document.getElementsByClassName("img-link");
+	for (var i = 0; i < img.length; i++) {
+		img[i].addEventListener("click", displayBox , false);
+	}
+
 };
