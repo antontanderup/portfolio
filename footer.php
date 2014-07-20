4d5b69f539beeb5ee48f6fece926e9b803249132
Anton Tanderup <antontandrup@gmail.com>
2014-07-20 13:06:51 +0200

    slight footer changes

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 	<footer>
-	 	<p>Copyright &copy; 2014 <a href="http://antontanderup.dk/">Anton Tanderup</a>.</p>
+	 	<p>Copyright &copy; 2014 <a href="http://antontanderup.dk/">Anton Tanderup</a></p>
 	</footer> <!-- footer END -->
 	<script src="js/min/behavior-ck.js"></script>
 </body>
