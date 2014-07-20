504354f2e15b9ae484e68f42f253efdfa15a62ca
Anton Tanderup <antontandrup@gmail.com>
2014-07-20 14:12:52 +0200

    Google Analytics

--- footer.php
+++ footer.php
@@ -3,5 +3,15 @@
 	 	<p>Copyright &copy; 2014 <a href="http://antontanderup.dk/">Anton Tanderup</a></p>
 	</footer> <!-- footer END -->
 	<script src="js/min/behavior-ck.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-8551255-6', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </body>
 </html>
