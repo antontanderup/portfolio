51956d13675e3e74f5092bda1662981409bbc5d8
Anton Tanderup <antontandrup@gmail.com>
2014-08-03 15:09:01 +0200

    Fixed link colors.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-	<footer>
+	<footer class="footer">
 	 	<p>Copyright &copy; 2014 <a href="http://antontanderup.dk/">Anton Tanderup</a></p>
 	</footer> <!-- footer END -->
 	<script src="js/min/behavior-ck.js"></script>
