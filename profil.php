e28f256409eda3601ecd7ca7ec44ff29cd7a3556
antontanderup <antontandrup@gmail.com>
2015-01-30 11:21:15 +0100

    Content change
    Changed profile text and tested out some new colors.

--- profil.php
+++ profil.php
@@ -13,20 +13,20 @@
 				<div class="skill"><span style="width:80%;">HTML(5) / CSS(3)</span></div>
 				<div class="skill"><span style="width:70%;">JavaScript</span></div>
 				<div class="skill"><span style="width:75%;">jQuery</span></div>
-				<p><i>Phonegap development, PHP, wordpress, joomla, responsive web design, mobile first, goal directed design, product photography.</i></p>
+				<p><i>Phonegap development, PHP, wordpress, joomla, responsive web design, mobile first, goal directed design, product photography, grafisk design, photoshop, illustrator, indesign.</i></p>
 			</section>
 		</aside> <!-- kompetencer END -->
 
 		<div class="intro">
 			<h1 class="title">Min profil</h1>
 			<section class="intro-text">
-				<p>B.A. International Communication &amp; Multimedia. Mine evner ligger indenfor web-design, front-end udvikling med en stærk interesse indenfor social media og de nyeste teknologier.</p>			
+				<p>Frontend udvikler hos Sporthamburg.dk. Mine evner ligger indenfor web-design, front-end udvikling med en stærk interesse indenfor social media og de nyeste teknologier.</p>			
 			</section>
 		</div> <!-- intro-text END -->
 
 		<article class="about">
 			<h2 class="subhead">Om mig</h2>
-			<p>Jeg er nyuddannet som Bachelor i International Kommunikation og Multimedie. Gennem min uddannelse har jeg fået indsigt i både tværkulturel kommunikation og marketing, og derudover færdigheder inden for webdesign og front-end udvikling.</p>
+			<p>Bachelor i International Kommunikation og Multimedie. Gennem min uddannelse har jeg fået indsigt i både tværkulturel kommunikation og marketing, og derudover færdigheder inden for webdesign og front-end udvikling.</p>
 			<p>Mine egne interesser ligger primært inden for grafisk arbejde, webdesign og front-end udvikling. Her er jeg stærk indenfor html og css, og i kraftig udvikling indenfor javascript. Derudover har jeg basis viden inden for PHP og mySQL som jeg også gerne vil videreudvikle. Senest er min interesse også vendt mod SEO som helt klart også er et felt jeg vil udvikle mig inden for.</p>
 			<p>Personligt er jeg noget af en fag nørd. Jeg følger med i de seneste trends på Smashing Magazine, A List Apart, CSS-tricks og mange andre kilder. Jeg holder mig opdateret indenfor de nyeste gadgets og teknologier der kan gøre mit liv nemmere og sjovere. Derudover er lidt af en science fiction nørd, og det er ikke sjældent at mine venner bliver udsat for diverse fantastiske referencer til diverse film og tv shows.</p>
 		</article> <!-- about END -->
